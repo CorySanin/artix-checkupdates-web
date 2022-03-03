3cd47191f4a2d9705468b4fcdf95101775ccee7e
Cory Sanin <corysanin@outlook.com>
2022-03-02 22:48:11 -0600

    Watch movable packages

--- index.js
+++ index.js
@@ -9,40 +9,23 @@ const EXTRASPACE = new RegExp('\\s+', 'g');
 
 let url;
 
-function notify(packarr) {
+function notify(packarr, type) {
     return phin({
         url,
         method: 'POST',
         data: {
+            type,
             packages: packarr.join('<br/>')
         }
     });
 }
 
-fs.readFile(PKGCONFIG, async (err, data) => {
-
-
-    if (err) {
-        console.log(err);
-    }
-    else {
-        data = JSON.parse(data);
-        const COMPAREPKG = data.COMPAREPKG || path.join(__dirname, 'comparepkg.txt');
-        const PREVIOUS = data.PREVIOUS || '/usr/volume/previous.json';
-        const packages = data.packages;
-        url = data.URL || 'http://localhost:8080/artix';
-        let linestart = -1, previous = [], movable = [];
-        try {
-            const p = JSON.parse(await fsp.readFile(PREVIOUS));
-            if ('packages' in p) {
-                previous = p.packages;
-            }
-        }
-        catch (ex) {
-            console.log(`Could not read ${PREVIOUS}: ${ex}`);
-        }
+function parseComparepkg(file, condition) {
+    return new Promise((res, reject) => {
+        let outarr = [];
+        let linestart = -1
         const rl = readline.createInterface({
-            input: fs.createReadStream(COMPAREPKG),
+            input: fs.createReadStream(file),
             output: process.stdout,
             terminal: false
         });
@@ -53,29 +36,71 @@ fs.readFile(PKGCONFIG, async (err, data) => {
             else {
                 line = line.substring(linestart).trim().replace(EXTRASPACE, ' ').split(' ', 3);
                 if (line[0] !== 'Arch') {
-                    const l = line[2];
-                    if (packages.indexOf(l) >= 0) {
-                        movable.push(l);
+                    if (condition(line)) {
+                        outarr.push(line[2]);
                     }
                 }
             }
         });
         rl.on('close', async () => {
-            let newpack = [];
-            try {
-                await fsp.writeFile(PREVIOUS, JSON.stringify({ packages: movable }));
+            res(outarr);
+        });
+    });
+}
+
+fs.readFile(PKGCONFIG, async (err, data) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        data = JSON.parse(data);
+        const COMPAREPKG = data.COMPAREPKG || path.join(__dirname, 'comparepkg.txt');
+        const MOVABLE = data.MOVABLE || path.join(__dirname, 'movable.txt');
+        const PREVIOUS = data.PREVIOUS || '/usr/volume/previous.json';
+        const packages = data.packages;
+        url = data.URL || 'http://localhost:8080/artix';
+        let previousm = [], previousu = [], movable = [], upgradable = [], newpack = [];
+        try {
+            const p = JSON.parse(await fsp.readFile(PREVIOUS));
+            if ('packages' in p) {
+                previousu = p.packages;
             }
-            catch (ex) {
-                console.log(`Could not write ${PREVIOUS}: ${ex}`);
+            if ('movable' in p) {
+                previousb = p.movable;
             }
-            movable.forEach(package => {
-                if (previous.indexOf(package) === -1) {
-                    newpack.push(package);
-                }
-            });
-            if (newpack.length > 0) {
-                await notify(newpack);
+        }
+        catch (ex) {
+            console.log(`Could not read ${PREVIOUS}: ${ex}`);
+        }
+
+        movable = await parseComparepkg(MOVABLE, line => line[0] !== line[1] && packages.indexOf(line[2]) >= 0);
+        upgradable = await parseComparepkg(COMPAREPKG, line => packages.indexOf(line[2]) >= 0);
+
+        try {
+            await fsp.writeFile(PREVIOUS, JSON.stringify({
+                packages: upgradable,
+                movable
+            }));
+        }
+        catch (ex) {
+            console.log(`Could not write ${PREVIOUS}: ${ex}`);
+        }
+        movable.forEach(package => {
+            if (previousm.indexOf(package) === -1) {
+                newpack.push(package);
             }
         });
+        if (newpack.length > 0) {
+            await notify(newpack, 'move');
+        }
+        newpack = [];
+        upgradable.forEach(package => {
+            if (previousu.indexOf(package) === -1) {
+                newpack.push(package);
+            }
+        });
+        if (newpack.length > 0) {
+            await notify(newpack, 'upgrade');
+        }
     }
 });
--- script.sh
+++ script.sh
@@ -2,7 +2,11 @@
 echo " ~cron job started~ "
 cd /usr/files
 buildtree -s
+comparepkg -m > movable.txt
+printf "\n=== MOVABLE ===\n"
+cat movable.txt
 comparepkg -u > comparepkg.txt
+printf "\n=== UPGRADABLE ===\n"
 cat comparepkg.txt
 node index.js
 echo "job finished."
