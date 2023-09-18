3ae0c1cfa4ac024e457ce21f8c884a42ddaa42f4
Cory Sanin <corysanin@outlook.com>
2023-09-17 19:18:06 -0500

    they *do* serve a purpose after all

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 echo "Artix Packy Notifier"
 echo "cron schedule: $CRON"
 mkdir -p /usr/volume/packages ~/.config/artix-checkupdates
-printf "ARTIX_MIRROR=$ARTIX_MIRROR\nARCH_MIRROR=$ARCH_MIRROR\nARTIX_REPOS=$ARTIX_REPOS\nARCH_REPOS=$ARCH_REPOS\n" > ~/.config/artix-checkupdates/config
+printf "ARTIX_MIRROR=$ARTIX_MIRROR\nARCH_MIRROR=$ARCH_MIRROR\nARTIX_REPOS=$ARTIX_REPOS\nARCH_REPOS=$ARCH_REPOS" "%s" "%s" > ~/.config/artix-checkupdates/config
 R=$(echo "$CRON" | sed "s/\\//\\\\\\//g")
 sed "s/%CRON%/$R/" cron > .cron
 crontab /etc/cron.d/.cron
